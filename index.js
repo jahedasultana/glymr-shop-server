b7ec3b58922290c638e1e1af4e993aadedfced6c
jaheda8256 <jahedasultana47@gmail.com>
2024-08-16 18:27:51 +0600

    added data in post

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(express.json())
 app.use(cors())
-// app.use(express.static("public"));
+
 
 // mongodb data  [change this data]
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
